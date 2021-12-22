3f0a356e86d1edb42387660c82e1740cb294d3df
Davy <davy.oger@yahoo.fr>
2021-12-22 11:35:47 +0100

    suppression routes Users

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,4 @@
-const User = require("./user.model");
 const Admins = require("./admins.models");
 const Contact = require("./contact.models");
 
-module.exports = { User, Admins, Contact };
+module.exports = { Admins, Contact };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 const mainRouter = require("express").Router();
-const usersRouter = require("./users.routes");
 const adminsRouter = require("./admins.routes");
 const contactRouter = require("./contact.routes");
 
 mainRouter.use("/contact", contactRouter);
-mainRouter.use("/users", usersRouter);
 mainRouter.use("/admins", adminsRouter);
 
 module.exports = mainRouter;
