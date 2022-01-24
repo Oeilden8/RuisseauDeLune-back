f9a6b59a1f2a8b718d6b64985d18d0ed97e86cc4
Davy <davy.oger@yahoo.fr>
2022-01-24 16:27:42 +0100

    modif base de données news

--- migrations/sqls/20220124095507-add-newsInsertions-up.sql
+++ migrations/sqls/20220124095507-add-newsInsertions-up.sql
@@ -1,4 +1,4 @@
 /* Replace with your SQL commands */
 
-INSERT INTO news (`title`, `actual_place`, `date_first`, `date_last`)
+INSERT INTO news (`title`, `places`, `date_first`, `date_last`)
 VALUES ("Goutte d'Eau", "La Loupe", "2020-02-20", "2022-05-21");
